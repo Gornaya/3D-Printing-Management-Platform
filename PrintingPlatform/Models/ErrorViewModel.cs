bd203592f21eeb19dda9d8494dd9d3f6925e15bb
Iryna Horna <gornairyna@gmail.com>
2026-02-18 18:00:07 -0600

    Recreate project as ASP.NET Core MVC

---
+++ PrintingPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PrintingPlatform.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
