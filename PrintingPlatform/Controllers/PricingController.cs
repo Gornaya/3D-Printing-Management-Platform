d6a483e41dac35036dfc1be9a24e7fa6eec081d9
Iryna Horna <gornairyna@gmail.com>
2026-04-07 21:30:42 -0500

    refactor: separate landing layout and fix page rendering

---
+++ PrintingPlatform/Controllers/PricingController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrintingPlatform.Controllers
+{
+    public class PricingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
